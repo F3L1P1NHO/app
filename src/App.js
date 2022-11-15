6a9c3a95cc2ab981a8f60c9cc74197ec5cc61e74
Felipe Santana <felipesantanasantos1812@gmail.com>
2022-11-15 12:22:51 -0300

    configurando_inicio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import * as React from 'react'
+import { View } from "react-native";
+import style from './styles';
+
+function App() {
+  return (
+    <View style={style.view_login}>
+
+    </View>
+  )
+}
+
+export default App
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+import { create } from "react-test-renderer";
+import React from "react";
+
+const style = StyleSheet.create({
+    view_login:{
+        flexGrow: 1,
+        backgroundColor: '#1C1C1C'
+    }
+})
+
+export default style
