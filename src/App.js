dc636aa46dcb462b0cb0071f519fc5ff30dcf5d8
F3L1P1NHO <99225765+F3L1P1NHO@users.noreply.github.com>
2022-11-15 11:40:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {Button, View, Alert, Text} from 'react-native'
+
+import estilo from './styles';
+
+
+function App() {
+
+
+}
+
+export default App;
+
