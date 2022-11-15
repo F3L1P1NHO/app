ea6a5673bab125da84f2d6b0ebb113313cc3ec57
Felipe Santana <felipesantanasantos1812@gmail.com>
2022-11-15 13:12:09 -0300

    EsbocoLogin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import * as React from 'react'
-import { TextInput, View } from "react-native";
+import { TextInput, View,Image } from "react-native";
 import style from './styles';
 import ButtonLogin from './components';
 
 function App() {
   return (
     <View style={style.view_login}>
+      <View style={style.ImageLogin}>
+        <Image 
+          source={require('../images/account.png')}
+        />
+      </View>
+      
+
       <TextInput placeholder='Email' keyboardType='email-address' style={style.txtInp}>
 
       </TextInput>
--- src/styles.js
+++ src/styles.js
@@ -3,27 +3,32 @@ import { create } from "react-test-renderer";
 import React from "react";
 
 const style = StyleSheet.create({
+    
     view_login:{
         flexGrow: 1,
         backgroundColor: '#1C1C1C',
         justifyContent: 'center',
         alignItems: 'center'
     },
+    ImageLogin:{
+        marginBottom: '5%'
+    },
     txtInp:{
         width: '70%',
         height: '7%',
         borderRadius: 30,
         backgroundColor: 'white',
-        marginTop: 15
+        marginTop: '5%'
     },
     Button_Login:{
         width: '70%',
         height: '8%',
         borderRadius: 30,
         backgroundColor: 'lightblue',
-        marginTop: '20%',
+        marginTop: '15%',
         justifyContent: 'center'
-    }
+    },
+    
 })
 
 export default style
